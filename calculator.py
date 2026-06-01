16213e53fc1134aadbdd4332e5357b383a0eaf77
swathireddy-05 <ihtawsydder@gmail.com>
2026-06-01 18:35:31 +0530

    Added calculator.py

---
+++ calculator.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Addition =",num1 + num2)
+print("subtraction =",num1 - num2)
+print("Multiplication =",num1 * num2)
+if num2 !=0 :
+  print("Division =",num1 / num2)
+else :
+  print("cannot divide by zero")
